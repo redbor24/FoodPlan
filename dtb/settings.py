9d2e5334a66458fa8d303099e5e3543e8e8ea277
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-03 19:25:13 +0900

    Добавил databaseurl

--- dtb/settings.py
+++ dtb/settings.py
@@ -98,10 +98,10 @@ ASGI_APPLICATION = 'dtb.asgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600,
-                                      default="sqlite:///db.sqlite3"
-                                      ),
+    'default': {}
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
